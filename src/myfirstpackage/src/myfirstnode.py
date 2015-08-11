a3008fc683bfe404626521f4c2cad03dffaadb27
gonzalo <gggonzalez002@ikasle.ehu.es>
2015-08-11 15:48:41 +0200

    Param modification

--- src/myfirstpackage/src/myfirstnode.py
+++ src/myfirstpackage/src/myfirstnode.py
@@ -6,10 +6,13 @@ rospy.init_node('myfirstnode')
 rate = rospy.Rate(10)
 
 # ADD YOUR CODE HERE
-variable = rospy.get_param("rosversion", 10)
-print "hello world running in ros version %s" % variable
-rospy.set_param("rosversion", 11)
-variable = rospy.get_param("rosversion", 1)
-print "hello world running in ros version %s" % variable
-
-rate.sleep()
+while not rospy.is_shutdown():
+    #variable = rospy.get_param("rosversion", 10)
+    #print "hello world running in ros version %s" % variable
+    #rospy.set_param("rosversion", 11)
+    #variable = rospy.get_param("rosversion", 1)
+    #print "hello world running in ros version %s" % variable
+    paramRate = rospy.get_param("rate", 10)
+    rate = rospy.Rate(paramRate)
+    print paramRate
+    rate.sleep()
