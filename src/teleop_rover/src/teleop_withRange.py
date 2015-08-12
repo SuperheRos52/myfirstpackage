9980427d174c9efbade90185da0fc9d9cdfba266
gonzalo <gggonzalez002@ikasle.ehu.es>
2015-08-12 16:45:48 +0200

    working version teleop

--- src/teleop_rover/src/teleop_withRange.py
+++ src/teleop_rover/src/teleop_withRange.py
@@ -7,36 +7,34 @@ from sensor_msgs.msg import Range
 
 rospy.init_node("teleop_withRange")
 pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+gJoy = Joy()
+gJoy.axes = [0,0,0,0,0,0]
+gRange = Range()
 twist = Twist()
 something_in_front = True
 
-def callback(joy):
-    global twist, pub, something_in_front
-    twist.angular.z = joy.axes[0]
-    if something_in_front:
-        twist.linear.x = 0
-    else:
-        twist.linear.x = joy.axes[1]
-    #pub.publish(twist)
+def callback_joy(joy):
+    global gJoy
+    gJoy = joy
 
 def callback_range(ranges):
-    global something_in_front, twist, pub
-    print "Object %f far away" % ranges.range
-    if ranges.range <= 0.20 and twist.linear.x>0:
-        twist.linear.x = 0
-        print "ACHTUNG!!!"
-        something_in_front = True
-    else:
-
-        something_in_front = False
-        #twist.linear.x = lastSpeed
-    #pub.publish(twist)
+    global gRange
+    gRange = ranges
 
 rospy.Subscriber('range', Range, callback_range)
-rospy.Subscriber('joy', Joy, callback)
+rospy.Subscriber('joy', Joy, callback_joy)
 r = rospy.Rate(20)
 
 #rospy.spin()
 while not rospy.is_shutdown():
+    print "Object %f far away" % gRange.range
+
+    twist.angular.z = gJoy.axes[0]
+    twist.linear.x = gJoy.axes[1]
+
+    if gRange.range <= 0.20 and gJoy.axes[1] >= 0:
+        twist.linear.x = 0
+        print "ACHTUNG!!!"
+
     pub.publish(twist)
     r.sleep()
