0353c80e63cad20fffe05f6ab8311ef7e4ce39e6
gonzalo <gggonzalez002@ikasle.ehu.es>
2015-08-12 15:20:07 +0200

    first optimistic version of teleop rover

---
+++ src/teleop_rover/src/teleop_node.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+rospy.init_node("teleop_node")
+pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+twist = Twist()
+
+def callback(joy):
+    global twist, pub
+    twist.linear.x = joy.axes[0]
+    twist.linear.y = joy.axes[1]
+    pub.publish(twist)
+
+rospy.Subscriber('joy', Joy, callback)
+
+rospy.spin()
